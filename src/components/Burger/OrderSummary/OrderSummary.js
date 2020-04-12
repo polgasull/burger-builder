060ae8dc9ccd3668cca3e9a075314c7545b037e3
polgasull <pgasulln@gmail.com>
2020-04-12 23:59:11 +0200

    Reordering folders

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientsSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+  render() {
+    const ingredientsSummary = Object.keys(this.props.ingredients)
     .map(ingKey => {
-      return <li key={ingKey}><span style={{textTransform: 'capitalize'}}> {ingKey} </span>: {props.ingredients[ingKey]} </li>
-    })
-  return (
-    <Aux>
-      <h3> Your Order </h3>
-      <p> A delicious burger with the following ingredients: </p>
-      <ul>
-        { ingredientsSummary }
-      </ul>
-      <p><strong> Total Price: {props.price.toFixed(2)} € </strong></p>
-      <p>Continue to checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Aux>
-  )
+      return ( 
+        <li key={ingKey}>
+          <span style={{textTransform: 'capitalize'}}> {ingKey} </span>: {this.props.ingredients[ingKey]} 
+        </li> 
+        );
+    });
+
+    return (
+      <Aux>
+        <h3> Your Order </h3>
+        <p> A delicious burger with the following ingredients: </p>
+        <ul>
+          { ingredientsSummary }
+        </ul>
+        <p><strong> Total Price: {this.props.price.toFixed(2)} € </strong></p>
+        <p>Continue to checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Aux>
+    )
+  }
 }
 
-export default orderSummary;
+export default OrderSummary;
